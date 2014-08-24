66418dd797615555c26e2b9383320d41f8ad193a
manisha1011 <manisha1011@users.noreply.github.com>
2014-08-24 21:52:39 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The code calculates the inverse of the matrix 'x'
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+# This function creates a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(x = matrix()) {           
+        i <- NULL
+        set <- function(y) {                            # Saves the input matrix and resets the inverse to NULL
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x                             # returns the value of original matrix
+        setinv <- function(inv) i <<- inv               # Stores value of inverse in i using superassignment
+        getinv <- function() i                          # returns value of i
+        list(set = set, get = get,                      # creates a list of all functions
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above 
+# If the inverse has already been calculated (and the matrix has not changed), then the cacheSolve should retrieve the inverse from the cache
+cacheSolve <- function(x, ...) {                         # The input is an object created by makeCacheMatrix function      
+        i <- x$getinv()                                 # Value of the inverse is obtained by accessing getinv function
+        if(!is.null(i)) {                               # If the inverse was already cached, message is printed and its value is returned
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()                                 # Else obtain the matrix
+        i <- solve(data,...)                            # Compute its inverse
+        x$setinv(i)                                     # Store the inverse matrix
+        i                                               # Return the inverse matrix
 }
